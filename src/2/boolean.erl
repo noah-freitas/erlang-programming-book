898da42a789cbc230fb523b7db1fd854c9f4d80f
Noah Freitas <noah.j.freitas@gmail.com>
2012-11-01 18:11:19 -0700

    Completed 2-3.
    Did not follow Hint at end of lesson (not sure how that would help.)
    
    My implementation uses a lot of wildcards `_` so all but `b_not/1` accept any time of value and will return false if given a non-boolean value.  I am not sure if this is a good idea.
    
    I could write write a `is_bool/1` function which would return true if the arguments is the atoms `true` or `false`.

--- src/2/boolean.erl
+++ src/2/boolean.erl
@@ -6,5 +6,25 @@ b_not(false) ->
 b_not(true) ->
 	false;
 b_not(_) ->
-	{error, unknown_boolean};
+	{error, unknown_boolean}.
 
+b_and(false, _) ->
+	false;
+b_and(true, true) ->
+	true;
+b_and(_, _) ->
+	false.
+
+b_or(true, _) ->
+	true;
+b_or(_, true) ->
+	true;
+b_or(_, _) ->
+	false.
+
+b_nand(true, false) ->
+	true;
+b_nand(false, true) ->
+	true;
+b_nand(_, _) ->
+	false.
