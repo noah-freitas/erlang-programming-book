204400fd20131e7586f6bf63c49651028da49daa
Noah Freitas <noah.j.freitas@gmail.com>
2012-11-08 08:32:28 -0800

    Completed merge_sort/1 for 3-6.
    This was a difficult exercise for me, both in terms of quicksort/1 and merge_sort/1.  I think a lot of the difficulty came from struggling with the algorithms themselves (Wikipedia was a help there) and then working out the recursion when implementing them.
    
    This completes 3-6.

--- src/3/sort.erl
+++ src/3/sort.erl
@@ -1,5 +1,14 @@
 -module(sort).
--export([quicksort/1]).
+-export([
+	quicksort/1,
+		% (List)
+		% sorts list ascending using quicksort.
+		% => SortedList
+	merge_sort/1
+		% (List)
+		% sorts list ascending using mergesort.
+		% => SortedList
+]).
 -import(manipulating_lists, [filter/2, concatenate/1]).
 
 quicksort([]) ->
@@ -7,3 +16,21 @@ quicksort([]) ->
 quicksort([H | T]) ->
 	Lows = filter(T, H),
 	concatenate([quicksort(Lows), [H], quicksort(T -- Lows)]).
+
+
+merge_sort(L) when length(L) < 2 ->
+	L;
+merge_sort(L) ->
+	{List1, List2} = lists:split(length(L) div 2, L),
+	merge(merge_sort(List1), merge_sort(List2), []).
+
+merge([], [], Merged) ->
+	lists:reverse(Merged);
+merge([H | T], [], Merged) ->
+	merge(T, [], [H | Merged]);
+merge([], [H | T], Merged) ->
+	merge([], T, [H | Merged]);
+merge([H1 | T1], [H2 | T2], Merged) when H1 =< H2 ->
+	merge(T1, [H2 | T2], [H1 | Merged]);
+merge(List1, [H | T], Merged) ->
+	merge(List1, T, [H | Merged]).
